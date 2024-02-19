83323673a59443dae8f40787affd959d23383b4f
Agus-Albarracin <abusscin_@hotmail.com>
2024-02-19 04:56:39 -0300

    Agus/ Se añade registro de talles a productos

--- server/src/models/productos.js
+++ server/src/models/productos.js
@@ -22,6 +22,10 @@ module.exports = (sequelize) => {
        type: DataTypes.FLOAT,
        allowNull: false,
     },
+    talle:{
+      type: DataTypes.FLOAT,
+      allowNull: true,
+    },
     stock:{
       type: DataTypes.JSONB,
       allowNull: false,

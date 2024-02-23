34d277e6d72ed143b08d05a0d2c6e762a2dbee78
Agus-Albarracin <abusscin_@hotmail.com>
2024-02-23 01:36:38 -0300

    Agus/ Se añade función para harcodear un usuario perma

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const server = require("./src/server");
 const { conn } = require('./src/db.js');
 const PORT = 3001;
-const dbRegisterDEMO = require("./dbRegistrerDEMO.js")
+const{dbRegisterDEMO, dbRegisterUsuariosDEMO} = require("./dbRegistrerDEMO.js")
 
 conn.sync({ force: true }).then(
 
     async() => { await dbRegisterDEMO();
+                 await dbRegisterUsuariosDEMO();
       
             server.listen(PORT, () => {
             console.log(`Server listening on port ${PORT}`);

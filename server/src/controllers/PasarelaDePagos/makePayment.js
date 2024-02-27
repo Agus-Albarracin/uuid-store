5d8e86cb9f8dfcdfba6e983f276503c2713e6bb5
SilviogPacheco <megaboy93mile@gmail.com>
2024-02-27 18:00:53 -0300

    agrego boton de pagar al carrito.Silvio

--- server/src/controllers/PasarelaDePagos/makePayment.js
+++ server/src/controllers/PasarelaDePagos/makePayment.js
@@ -1,38 +1,37 @@
 require("dotenv").config();
-const { MercadoPagoConfig, Preference } = require('mercadopago');
-const {TOKEN_MP} = process.env;
+const { MercadoPagoConfig, Preference } = require("mercadopago");
+const { TOKEN_MP } = process.env;
 
 const client = new MercadoPagoConfig({ accessToken: TOKEN_MP });
 
 const makePayment = async (req, res) => {
-    try {
-        const body = {
-          items: [
-            {
-              title: req.body.title,
-              quantity: Number(req.body.quantity),
-              unit_price: Number(req.body.price),
-              currency_id: "ARS",
-            },
-          ],
-          back_urls: {
-            success: "https://google.com",
-            failure: "https://google.com",
-            pending: "https://google.com",
-          },
-          auto_return: "approved",
-        };
-        const preference = new Preference(client);
-        const result = await preference.create({body})
-        res.json({
-            id: result.id
-        })
-      } catch (error) {
-        console.log(error)
-        res.status(500).json({
-            error: 'Error al crear la preferencia'
-        })
-      }
-}
+  try {
+    const items = req.body.cart.map((ele) => ({
+      title: ele.nombre,
+      quantity: 1,
+      unit_price: Number(ele.precio),
+      currency_id: "ARS",
+    }));
+    const body = {
+      items,
+      back_urls: {
+        success: "http://127.0.0.1:5174/",
+        failure: "http://127.0.0.1:5174/",
+        pending: "http://127.0.0.1:5174/",
+      },
+      auto_return: "approved",
+    };
+    const preference = new Preference(client);
+    const result = await preference.create({ body });
+    res.json({
+      id: result.id,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: "Error al crear la preferencia",
+    });
+  }
+};
 
-module.exports = makePayment
+module.exports = makePayment;

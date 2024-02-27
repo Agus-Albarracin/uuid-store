55026f20b579c7eacf254c5ad24fe1ba01c9e042
Agus-Albarracin <abusscin_@hotmail.com>
2024-02-27 14:27:14 -0300

    Agus/ se modifico para que se pueda mostrar solo los talles en stock y/o SIN STOCK+

--- client/src/Views/Detail/Detail.jsx
+++ client/src/Views/Detail/Detail.jsx
@@ -1,4 +1,3 @@
-// import style from "./Detail.module.css";
 import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 
@@ -28,7 +27,7 @@ const Detail = () => {
   useEffect(() => {
     dispatch(getDetail(id));
     // return () => {
-    //     console.log(detail);
+    //     console.log(detail); 
     //     dispatch(clearDetail());
     // };
   }, [dispatch, id]);
@@ -39,11 +38,10 @@ const Detail = () => {
   };
 
   return (
-
     <div className="py-6 bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center space-x-2 text-gray-400 text-sm">
-          <Link to="/" className="hover:underline hover:text-gray-600">Home</Link>
+          <Link to="/" className="hover:underline hover:text-gray-600">Inicio</Link>
           <span>
             <svg className="h-5 w-5 leading-none text-red-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7" />
@@ -63,9 +61,9 @@ const Detail = () => {
           <div className="md:flex-1 px-4">
             <div className={`h-64 md:h-80 rounded-lg bg-gray-100 mb-4 flex items-center justify-center`}>
               {detail.imagen ? (
-                <img src={detail.imagen[image - 1]} alt={`Image ${image}`} className="w-full h-full object-cover" />
+                <img src={detail.imagen[image - 1]} alt={`Imagen ${image}`} className="w-full h-full object-cover" />
               ) : (
-                <img src={images[image - 1]} alt={`Image ${image}`} className="w-full h-full object-cover" />
+                <img src={images[image - 1]} alt={`Imagen ${image}`} className="w-full h-full object-cover" />
               )}
             </div>
 
@@ -73,7 +71,7 @@ const Detail = () => {
               {(detail.imagen || images).map((img, i) => (
                 <div key={i} className="flex-1 px-2">
                   <button onClick={() => changeImage(i + 1)} className={`focus:outline-none w-full rounded-lg h-24 md:h-32 bg-gray-100 flex items-center justify-center ${image === i + 1 ? 'ring-2 ring-indigo-300 ring-inset' : ''}`}>
-                    <img src={img} alt={`Thumbnail ${i + 1}`} className="w-full h-full object-cover rounded-lg" />
+                    <img src={img} alt={`Miniatura ${i + 1}`} className="w-full h-full object-cover rounded-lg" />
                   </button>
                 </div>
               ))}
@@ -81,7 +79,7 @@ const Detail = () => {
           </div>
           <div className="md:flex-1 px-4">
             <h2 className="mb-2 leading-tight tracking-tight font-bold text-gray-800 text-2xl md:text-3xl">{detail?.nombre}</h2>
-            <p className="text-gray-500 text-sm">By <a href="#" className="text-red-500 hover:underline">{detail?.marca}</a></p>
+            <p className="text-gray-500 text-sm">Por <a href="#" className="text-red-500 hover:underline">{detail?.marca}</a></p>
 
             <div className="flex items-center space-x-4 my-4">
               <div>
@@ -91,11 +89,11 @@ const Detail = () => {
                 </div>
               </div>
               <div className="flex-1">
-                <p className="text-green-500 text-xl font-semibold">Save 12%</p>
-                <p className="text-gray-400 text-sm">Inclusive of all Taxes.</p>
+                <p className="text-green-500 text-xl font-semibold">Ahorra 12%</p>
+                <p className="text-gray-400 text-sm">Incluye todos los impuestos.</p>
               </div>
             </div>
-            <table className="table-auto border-collapse w-full bg-white shadow-md mb-4">
+            {/* <table className="table-auto border-collapse w-full bg-white shadow-md mb-4">
               <tbody>
                 <tr className="border-b">
                   <td className="py-2 px-4 font-semibold text-red-500">Tallas disponibles:</td>
@@ -115,28 +113,26 @@ const Detail = () => {
                   )}
                 </tr>
               </tbody>
-            </table>
+            </table> */}
             <p className="text-gray-500">Lorem ipsum, dolor sit, amet consectetur adipisicing elit. Vitae exercitationem porro saepe ea harum corrupti vero id laudantium enim, libero blanditiis expedita cupiditate a est.</p>
 
             <div className="flex py-4 space-x-4">
               <div className="relative">
                 <div className="text-center left-0 pt-2 right-0 absolute block text-xs uppercase text-gray-400 tracking-wide font-semibold">
-                  <span className="ml-auto">TALLAS</span>
+                  <span className="ml-auto">Tallas</span>
                 </div>
                 <select className="cursor-pointer appearance-none rounded-xl border border-gray-200 pl-4 pr-8 h-14 ml-2 flex items-end pb-1 w-auto sm:w-32">
-                  {typeof detail.stock === 'object' ? (
-                    Object.entries(detail.stock).map(([talle]) => (
-                      <option key={detail.stock[1]} className="text-center">{talle}</option>
-                    ))
-                  ) : (
-                    <>
-                      <option className="text-center">41</option>
-                      <option className="text-center">42</option>
-                      <option className="text-center">43</option>
-                      <option className="text-center">44</option>
-                      <option className="text-center">45</option>
-                    </>
-                  )}
+                {typeof detail.stock === 'object' 
+                
+                ? ( Object.entries(detail.stock).filter(([talle, cantidad]) => cantidad > 0).length > 0 
+
+                     ? (Object.entries(detail.stock).filter(([talle, cantidad]) => cantidad > 0)
+                     .map(([talle]) => (<option key={detail.stock[1]} className="text-center">{talle}</option>))) 
+                     
+                     : ( <option className="text-center">SIN STOCK</option> )
+                     
+              ) : (null, "error en cargar el stock")
+              }
 
                 </select>
 
@@ -147,7 +143,7 @@ const Detail = () => {
 
               <button type="button" className="h-14 px-6 py-2 font-semibold rounded-xl bg-red-500 hover:bg-red-400 text-white" 
               onClick={handleAddToCart}>
-                Add to Cart
+                Agregar al carrito
               </button>
             </div>
           </div>

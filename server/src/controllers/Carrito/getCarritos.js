58b8ae3705681d5573ba53f3653fd27c557bcf17
Agus-Albarracin <abusscin_@hotmail.com>
2024-03-04 22:59:55 -0300

    Agus/ Se añade función de buscar carritos por id

--- server/src/controllers/Carrito/getCarritos.js
+++ server/src/controllers/Carrito/getCarritos.js
@@ -15,4 +15,19 @@ const getCarritos = async (req, res) =>{
     }
 }
 
-module.exports = {getCarritos}
+const getCarritosById = async (req, res) =>{
+    const {idDeCompra} = req.body
+    try{
+        let carritoid = await Carrito.findByPk(idDeCompra)
+        if (!carritoid) return res.status(400).json("No hay carrito con ese id")
+        carritoid = carritoid.get();
+
+    return res.status(200).json(carritoid)
+
+    }catch(error){
+
+    return res.status(500).json({message: error.message})
+    }
+}
+
+module.exports = {getCarritos, getCarritosById}

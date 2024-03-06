9ff6cf367bcfa5bb30b1d1c9450040373bf0f5b2
Agus-Albarracin <abusscin_@hotmail.com>
2024-03-05 21:33:02 -0300

    Agus/ Se agrega section para ver los usuarios, y se agrego la función para poder eliminarlos

--- client/src/components/AdminComponents/AllUsers/AllUsers.jsx
+++ client/src/components/AdminComponents/AllUsers/AllUsers.jsx
@@ -1,48 +1,46 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from 'react';
-import { allUsers } from "../../../redux/actions";
-
+import { useEffect } from "react";
+import { allUsers, deleteUser } from "../../../redux/actions";
 
 const AllUsers = () => {
-    
-    const users = useSelector((state) => state.allUsers);
-    const dispatch = useDispatch();
+  const users = useSelector((state) => state.allUsers);
+  const dispatch = useDispatch();
 
-    useEffect(() =>{
-        dispatch(allUsers())
-    },[])
+  useEffect(() => {
+    dispatch(allUsers());
+  }, []);
 
-    return (
-        <div className="contenedor-table">
-                <table>
-                    <thead>
-                        <tr>
-                            <th> Nombre </th>
-                            <th> Email </th>
-                            <th> Accion </th>
-                        </tr>
-                    </thead>
-                    
-                    <tbody>
-                        { users.map ( (user) => {
-                            <tr key={user.id}>
-                                <td>{user.nombre}</td>
-                                <td>{user.email}</td>
-                                <td><button> Eliminar </button></td>
-                                
-                            </tr>
-                            }
-                        )}
-                                           
-                    </tbody>
-                    
-                </table>
-                
-            </div>
-            
-    )
-    
-}
+  const handleDeleteUser = (email) => {
+    console.log("Correo electrónico del usuario a eliminar:", email);
+    dispatch(deleteUser(email)); 
+  };
+
+  return (
+    <div className="contenedor-table">
+      <table>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Email</th>
+            <th>Acción</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.nombre}</td>
+              <td>{user.email}</td>
+              <td>
+                <button onClick={() => handleDeleteUser(user.email)}>Eliminar</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default AllUsers;

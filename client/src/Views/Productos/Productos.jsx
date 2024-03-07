99e17475fdff4af79e488bd9a480f77ec610489e
Alejod951 <Alejod951@gmail.com>
2024-03-07 14:45:18 -0500

    Todos los productos con paguinado

--- client/src/Views/Productos/Productos.jsx
+++ client/src/Views/Productos/Productos.jsx
@@ -1,32 +1,42 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getProductos, getName } from "../../redux/actions";
-import SideBar from "../../components/SideBar/SideBar"
+import SideBar from "../../components/SideBar/SideBar";
 import Cards from "../../components/Cards/Cards";
 
 import "./Productos.css";
 
-function Productos() {
+const Productos = () => {
   const dispatch = useDispatch();
   const allProductos = useSelector((state) => state.allProductos);
   const [searchString, setSearchString] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const productsPerPage = 10;
 
-  function handleChange(e) {
+  const handleChange = (e) => {
     e.preventDefault();
     setSearchString(e.target.value);
-  }
+  };
 
-  function handleSubmit(e) {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(getName(searchString))
-  }
+    dispatch(getName(searchString));
+  };
+
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
 
   useEffect(() => {
     if (allProductos.length === 0) {
       dispatch(getProductos());
-
     }
   }, [dispatch, allProductos.length]);
+
+  // Calculate products to display based on pagination
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = allProductos.slice(indexOfFirstProduct, indexOfLastProduct);
 
   return (
     <div className="flex flex-col md:flex-row h-screen overflow-hidden">
@@ -34,11 +44,23 @@ function Productos() {
         <SideBar handleChange={handleChange} handleSubmit={handleSubmit} />
       </div>
       <div className="w-full p-4 overflow-y-auto">
-        <Cards data={allProductos} />
+        <Cards data={currentProducts} />
+        
+        {/* Pagination buttons */}
+        <div className="flex justify-center mt-4">
+          {Array.from({ length: Math.ceil(allProductos.length / productsPerPage) }).map((_, index) => (
+            <button
+              key={index}
+              className={`mx-2 px-3 py-1 border ${currentPage === index + 1 ? 'bg-gray-500 text-white' : 'bg-white'}`}
+              onClick={() => handlePageChange(index + 1)}
+            >
+              {index + 1}
+            </button>
+          ))}
+        </div>
       </div>
     </div>
   );
-
-}
+};
 
 export default Productos;

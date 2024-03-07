1477a95a9bd6f4ecd4c23245f969ea6095b0fb2c
Alejod951 <Alejod951@gmail.com>
2024-03-06 23:39:49 -0500

    Corregir el detail, si tiene o no descuento renderizar en descuento y hacer la operacion del descuento

--- client/src/Views/Detail/Detail.jsx
+++ client/src/Views/Detail/Detail.jsx
@@ -52,7 +52,7 @@ const Detail = () => {
 
   // const handleRating = (value) => {
   //   setRating(value);
-    
+
   //   console.log(`Calificación: ${value}`);
   // };
 
@@ -67,94 +67,94 @@ const Detail = () => {
 
     }
   };
-    return (
-      <div className="py-6 bg-white">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center space-x-2 text-gray-400 text-sm">
-            <Link to="/" className="hover:underline hover:text-gray-600">
-              Inicio
-            </Link>
-            <span>
-              <svg
-                className="h-5 w-5 leading-none text-red-500"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M9 5l7 7-7 7"
-                />
-              </svg>
-            </span>
-            <a href="#" className="hover:underline hover:text-gray-600">
-              {detail?.marca}
-            </a>
-            <span>
-              <svg
-                className="h-5 w-5 leading-none text-red-500"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M9 5l7 7-7 7"
-                />
-              </svg>
-            </span>
-            <span>{detail?.nombre}</span>
-          </div>
+  return (
+    <div className="py-6 bg-white">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center space-x-2 text-gray-400 text-sm">
+          <Link to="/" className="hover:underline hover:text-gray-600">
+            Inicio
+          </Link>
+          <span>
+            <svg
+              className="h-5 w-5 leading-none text-red-500"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M9 5l7 7-7 7"
+              />
+            </svg>
+          </span>
+          <a href="#" className="hover:underline hover:text-gray-600">
+            {detail?.marca}
+          </a>
+          <span>
+            <svg
+              className="h-5 w-5 leading-none text-red-500"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M9 5l7 7-7 7"
+              />
+            </svg>
+          </span>
+          <span>{detail?.nombre}</span>
         </div>
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-6">
-          <div className="flex flex-col md:flex-row -mx-4">
-            <div className="md:flex-1 px-4">
-              <div
-                className={`h-64 md:h-80 rounded-lg bg-gray-100 mb-4 flex items-center justify-center`}
-              >
-                {detail.imagen ? (
-                  <img
-                    src={detail.imagen[image - 1]}
-                    alt={`Imagen ${image}`}
-                    className="w-full h-full object-cover"
-                  />
-                ) : (
-                  <img
-                    src={images[image - 1]}
-                    alt={`Imagen ${image}`}
-                    className="w-full h-full object-cover"
-                  />
-                )}
-              </div>
-
-              <div className="flex -mx-2 mb-4">
-                {(detail.imagen || images).map((img, i) => (
-                  <div key={i} className="flex-1 px-2">
-                    <button
-                      onClick={() => changeImage(i + 1)}
-                      className={`focus:outline-none w-full rounded-lg h-24 md:h-32 bg-gray-100 flex items-center justify-center ${image === i + 1 ? "ring-2 ring-indigo-300 ring-inset" : ""
-                        }`}
-                    >
-                      <img
-                        src={img}
-                        alt={`Miniatura ${i + 1}`}
-                        className="w-full h-full object-cover rounded-lg"
-                      />
-                    </button>
-                  </div>
-                ))}
-              </div>
+      </div>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-6">
+        <div className="flex flex-col md:flex-row -mx-4">
+          <div className="md:flex-1 px-4">
+            <div
+              className={`h-64 md:h-80 rounded-lg bg-gray-100 mb-4 flex items-center justify-center`}
+            >
+              {detail.imagen ? (
+                <img
+                  src={detail.imagen[image - 1]}
+                  alt={`Imagen ${image}`}
+                  className="w-full h-full object-cover"
+                />
+              ) : (
+                <img
+                  src={images[image - 1]}
+                  alt={`Imagen ${image}`}
+                  className="w-full h-full object-cover"
+                />
+              )}
             </div>
-            <div className="md:flex-1 px-4">
-              <h2 className="mb-2 leading-tight tracking-tight font-bold text-gray-800 text-2xl md:text-3xl">
-                {detail?.nombre}
-                {/* <div className="flex items-center">
+
+            <div className="flex -mx-2 mb-4">
+              {(detail.imagen || images).map((img, i) => (
+                <div key={i} className="flex-1 px-2">
+                  <button
+                    onClick={() => changeImage(i + 1)}
+                    className={`focus:outline-none w-full rounded-lg h-24 md:h-32 bg-gray-100 flex items-center justify-center ${image === i + 1 ? "ring-2 ring-indigo-300 ring-inset" : ""
+                      }`}
+                  >
+                    <img
+                      src={img}
+                      alt={`Miniatura ${i + 1}`}
+                      className="w-full h-full object-cover rounded-lg"
+                    />
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="md:flex-1 px-4">
+            <h2 className="mb-2 leading-tight tracking-tight font-bold text-gray-800 text-2xl md:text-3xl">
+              {detail?.nombre}
+              {/* <div className="flex items-center">
                   {[1, 2, 3, 4, 5].map((value) => (
                     <button
                       key={value}
@@ -166,105 +166,108 @@ const Detail = () => {
                     </button>
                   ))}
                 </div> */}
-              </h2>
-              <p className="text-gray-500 text-sm">
-                Por{" "}
-                <a href="#" className="text-red-500 hover:underline">
-                  {detail?.marca}
-                </a>
-              </p>
+            </h2>
+            <p className="text-gray-500 text-sm">
+              Por{" "}
+              <a href="#" className="text-red-500 hover:underline">
+                {detail?.marca}
+              </a>
+            </p>
 
-              <div className="flex items-center space-x-4 my-4">
-                <div>
-                  <div className="rounded-lg bg-gray-100 flex py-2 px-3">
-                    <span className="text-red-500 mr-1 mt-1">$</span>
-                    <span className="font-bold text-red-500 text-3xl">
-                      {detail?.precio}
-                    </span>
-                  </div>
-                </div>
-                <div className="flex-1">
-                  <p className="text-green-500 text-xl font-semibold">
-                    Ahorra 12%
-                  </p>
-                  <p className="text-gray-400 text-sm">
-                    Incluye todos los impuestos.
-                  </p>
+            <div className="flex items-center space-x-4 my-4">
+              <div>
+                <div className="rounded-lg bg-gray-100 flex py-2 px-3">
+                  <span className="text-red-500 mr-1 mt-1">$</span>
+                  <span className="font-bold text-red-500 text-3xl">
+                    {detail?.precio-(detail?.precio*detail?.enDescuento)}
+                  </span>
                 </div>
               </div>
-      
-              <p className="text-gray-500">
-                Lorem ipsum, dolor sit, amet consectetur adipisicing elit. Vitae
-                exercitationem porro saepe ea harum corrupti vero id laudantium
-                enim, libero blanditiis expedita cupiditate a est.
-              </p>
+              <div className="flex-1">
+                {detail?.enDescuento !== 0 && (
+                  <p className="text-green-500 text-xl font-semibold">
+                    {detail?.enDescuento}
+                  </p>
+                )}
 
-              <div>
-                <p><b>Selecciona tu talla:</b></p>
-                <div className="flex-wrap space-x-2">
-                  {typeof detail.stock === "object" ? (
-                    Object.entries(detail.stock).filter(([_, cantidad]) => cantidad > 0).length > 0 ? (
-                      Object.entries(detail.stock)
-                        .filter(([_, cantidad]) => cantidad > 0)
-                        .map(([talle], index) => (
-                          <button
-                            key={index}
-                            className={`py-2 px-4 m-1 focus:outline-none border border-gray-300 rounded-md transition duration-300 ease-in-out ${talle === tallaSeleccionada
-                              ? 'bg-blue-500 text-white'
-                              : 'bg-transparent text-gray-700 hover:bg-gray-200'
-                              }`}
-                            onClick={() => handleSeleccionarTalla(talle)}
-                          >
-                            {talle}
-                          </button>
-                        ))
-                    ) : (
-                      <button
-                        className="py-2 px-4 border border-gray-300 text-gray-700 rounded-md cursor-not-allowed opacity-50"
-                        disabled
-                      >
-                        SIN STOCK
-                      </button>
-                    )
+                <p className="text-gray-400 text-sm">
+                  Incluye todos los impuestos.
+                </p>
+              </div>
+            </div>
+
+            <p className="text-gray-500">
+              Lorem ipsum, dolor sit, amet consectetur adipisicing elit. Vitae
+              exercitationem porro saepe ea harum corrupti vero id laudantium
+              enim, libero blanditiis expedita cupiditate a est.
+            </p>
+
+            <div>
+              <p><b>Selecciona tu talla:</b></p>
+              <div className="flex-wrap space-x-2">
+                {typeof detail.stock === "object" ? (
+                  Object.entries(detail.stock).filter(([_, cantidad]) => cantidad > 0).length > 0 ? (
+                    Object.entries(detail.stock)
+                      .filter(([_, cantidad]) => cantidad > 0)
+                      .map(([talle], index) => (
+                        <button
+                          key={index}
+                          className={`py-2 px-4 m-1 focus:outline-none border border-gray-300 rounded-md transition duration-300 ease-in-out ${talle === tallaSeleccionada
+                            ? 'bg-blue-500 text-white'
+                            : 'bg-transparent text-gray-700 hover:bg-gray-200'
+                            }`}
+                          onClick={() => handleSeleccionarTalla(talle)}
+                        >
+                          {talle}
+                        </button>
+                      ))
                   ) : (
                     <button
-                      className="py-2 px-4 border border-red-500 text-red-500 rounded-md cursor-not-allowed opacity-50"
+                      className="py-2 px-4 border border-gray-300 text-gray-700 rounded-md cursor-not-allowed opacity-50"
                       disabled
                     >
-                      Error en cargar el stock
+                      SIN STOCK
                     </button>
-                  )}
-                </div>
-
-
-
-                {tallaSeleccionada && (
-                  <p>Talla seleccionada: {tallaSeleccionada}</p>
+                  )
+                ) : (
+                  <button
+                    className="py-2 px-4 border border-red-500 text-red-500 rounded-md cursor-not-allowed opacity-50"
+                    disabled
+                  >
+                    Error en cargar el stock
+                  </button>
                 )}
               </div>
 
-              <div className="flex py-4 space-x-4">
-                <div className="relative">
-       
 
 
-                </div>
+              {tallaSeleccionada && (
+                <p>Talla seleccionada: {tallaSeleccionada}</p>
+              )}
+            </div>
 
-                <button
-                  type="button"
-                  className="h-14 px-6 py-2 font-semibold rounded-xl bg-red-500 hover:bg-red-400 text-white"
-                  onClick={handleAddToCart}
-                >
-                  Agregar al carrito
-                </button>
+            <div className="flex py-4 space-x-4">
+              <div className="relative">
+
+
+
               </div>
+
+              <button
+                type="button"
+                className="h-14 px-6 py-2 font-semibold rounded-xl bg-red-500 hover:bg-red-400 text-white"
+                onClick={handleAddToCart}
+              >
+                Agregar al carrito
+              </button>
             </div>
           </div>
         </div>
-        <ProductReview ></ProductReview>
       </div>
-    );
-  };
+      <ProductReview ></ProductReview>
+    </div>
+  );
+};
 
 
 export default Detail;

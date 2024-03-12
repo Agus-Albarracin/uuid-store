4cf03107d5be32fef2cee0cdd5c1691149694332
Agus-Albarracin <abusscin_@hotmail.com>
2024-03-11 21:14:13 -0300

    Agus/ Se añade el azul al boton

--- client/src/components/Header/MenuUser/MenuUser.jsx
+++ client/src/components/Header/MenuUser/MenuUser.jsx
@@ -57,7 +57,10 @@ const MenuUser = ({ mostrarUser }) => {
           <div className="text-black">{`${user.nombre}  ${user.apellido}`}</div>
           {user.admin && (
             <Link to="/admin">
-              <span className="text red"> Panel de Admin </span>
+              <span 
+              className="cursor-pointer text-blue-500 mr-4"
+              
+              > Panel de Admin </span>
             </Link>
           )}
           <Link to="/user">

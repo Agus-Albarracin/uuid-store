35bc318b77c42abda59ee4931385ff5656a12c9b
Agus-Albarracin <abusscin_@hotmail.com>
2024-03-12 00:18:04 -0300

    M 5

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './**/*.{html,js,jsx,ts,tsx}', // Incluir todos los archivos HTML, JS, JSX, TS y TSX en todo el proyecto
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
-};
+ 
+}
+

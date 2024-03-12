f1b73954847b5b39d8446ae24f7ec61f5de89110
Agus-Albarracin <abusscin_@hotmail.com>
2024-03-12 16:09:48 -0300

    M29(2)

--- client/src/assets/data.js
+++ client/src/assets/data.js
@@ -1,22 +1,24 @@
+const baseUrl = 'https://uuid-store.vercel.app/assets/';
+
 export const data = [
   {
     id: 1, 
-    imgUrl: 'https://uuid-store.vercel.app/src/assets/cover1.png'
+    imgUrl: baseUrl + 'cover1.png'
   },
   {
     id: 2, 
-    imgUrl: 'https://uuid-store.vercel.app/src/assets/cover2.jpg'
+    imgUrl: baseUrl + 'cover2.jpg'
   },
   {
     id: 3, 
-    imgUrl: 'https://uuid-store.vercel.app/src/assets/cover1.png'
+    imgUrl: baseUrl + 'cover1.png'
   },
   {
     id: 4, 
-    imgUrl: 'https://uuid-store.vercel.app/src/assets/cover2.jpg'
+    imgUrl: baseUrl + 'cover2.jpg'
   },
   {
     id: 5, 
-    imgUrl: 'https://uuid-store.vercel.app/src/assets/cover1.png'
+    imgUrl: baseUrl + 'cover1.png'
   }
 ];

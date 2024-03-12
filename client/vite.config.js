1256c2d06774faad8bae87ccd2bb0322c1f80569
Agus-Albarracin <abusscin_@hotmail.com>
2024-03-12 01:56:29 -0300

    M15

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs';
+
+// Función para verificar si un archivo existe
+const fileExists = (filePath) => {
+  try {
+    fs.accessSync(filePath);
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
+
+// Variable para controlar si index.css se importó
+let indexCssImported = false;
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,8 +21,11 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import '/styles/main.scss';`,
+        additionalData: fileExists('/styles/index.css') && !indexCssImported ? `@import '/styles/index.css';` : '',
       },
-    }
-  }
-})
+    },
+  },
+});
+
+// Marcar index.css como importado después de la primera importación
+indexCssImported = true;

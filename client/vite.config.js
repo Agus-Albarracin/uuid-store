9df367acb7fd9bf7316906637e9cf49ef1b4469f
Agus-Albarracin <abusscin_@hotmail.com>
2024-03-12 02:06:21 -0300

    M16

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,36 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import WindiCSS from 'vite-plugin-windicss';
-import fs from 'fs';
-
-// Función para verificar si un archivo existe
-const fileExists = (filePath) => {
-  try {
-    fs.accessSync(filePath);
-    return true;
-  } catch (err) {
-    return false;
-  }
-};
-
-// Variable para controlar si index.css se importó
-let indexCssImported = false;
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    WindiCSS()
-  ],
+  plugins: [react()],
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData() {
-          const importIndexCss = fileExists('/styles/index.css') && !indexCssImported;
-          indexCssImported = true;
-          return importIndexCss ? `@import '/styles/index.css';` : '';
-        },
+        additionalData: `@import '/styles/main.scss';`,
       },
-    },
-  },
-});
+    }
+  }
+})

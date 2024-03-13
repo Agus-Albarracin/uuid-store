19b76b274874fb2b5b8cb1e1d8a7a48ecb678809
Agus-Albarracin <abusscin_@hotmail.com>
2024-03-13 01:09:45 -0300

    Agus/ se quito el scrollbar y se añadio botón de ver más

--- client/src/Views/Productos/Productos.jsx
+++ client/src/Views/Productos/Productos.jsx
@@ -44,7 +44,7 @@ function Productos() {
            <button onClick={handleVerMas} className="mt-4 bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
            Ver más
            </button>
-          </div>
+              </div>
 
 
         )}

f3fa20bfbc124aede55b9d613ab73352750988b6
QuemeraT <adrianortizzt1vl@gmail.com>
2024-03-14 22:58:36 -0300

    Adri / se muestra la información del usuario cuando se entra a la view

--- client/src/Views/User/DataUser.jsx
+++ client/src/Views/User/DataUser.jsx
@@ -1,59 +1,61 @@
 import { useSelector } from "react-redux";
-import { useState } from 'react';
+import { useState } from "react";
 import InfoUser from "../../components/InfoUser/InfoUser";
 import MisCompras from "../../components/MisCompras/MisCompras";
 
-import "./DataUser.css"
+import "./DataUser.css";
 
-function DataUser(){
+function DataUser() {
+  const user = useSelector((state) => state.actualUser);
+  console.log(user);
 
-    const user = useSelector((state) => state.actualUser);
-    console.log(user);
-    
-    const [view, setView] = useState('estadisticas')
+  const [view, setView] = useState("info");
 
-    const handleView = (option) => {
-        setView(option);
-    }
+  const handleView = (option) => {
+    setView(option);
+  };
 
-    return(
-        <div>
-            <div className="contenedor">
-                <div className="contenedor3">
-                    <div className="card1">
-                        <h1>Hola!{" "}{user.nombre}</h1>
-                    </div>
+  return (
+    <div>
+      <div className="contenedor">
+        <div className="contenedor3">
+          <div className="card1">
+            <h1>Hola! {user.nombre}</h1>
+          </div>
 
-                    <div className="card" onClick={() => handleView('info')}>
-                        <span >
-                            <strong>Informacion Personal</strong>
-                        </span>              
-                    </div>
-                                  
-                    <div className="card" onClick={() => handleView('compras')}>
-                        <span >
-                            <strong>Mis compras</strong>
-                        </span>
-                    </div>                 
-                </div>
+          <div className="card" onClick={() => handleView("info")}>
+            <span>
+              <strong>Informacion Personal</strong>
+            </span>
+          </div>
 
-                {view === 'info' && (
-                    <article className="contenedor2">
-                        <strong><h1> -INFORMACION PERSONAL- </h1></strong>
-                        <InfoUser />
-                    </article>
-                )}
-
-                {view === 'compras' && (
-                    <article className="contenedor2">
-                        <strong><h2> MIS COMPRAS </h2></strong>
-                        <MisCompras />
-                    </article>
-                )}
-
-            </div>
+          <div className="card" onClick={() => handleView("compras")}>
+            <span>
+              <strong>Mis compras</strong>
+            </span>
+          </div>
         </div>
-    )
+
+        {view === "info" && (
+          <article className="contenedor2">
+            <strong>
+              <h1> -INFORMACION PERSONAL- </h1>
+            </strong>
+            <InfoUser />
+          </article>
+        )}
+
+        {view === "compras" && (
+          <article className="contenedor2">
+            <strong>
+              <h2> MIS COMPRAS </h2>
+            </strong>
+            <MisCompras />
+          </article>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default DataUser;

f2099933ee27eff777f53af993db02b0b581af0f
QuemeraT <adrianortizzt1vl@gmail.com>
2024-03-15 00:36:32 -0300

    Adri / modifique el controller para que se actualice bien el prodcuto

--- server/src/controllers/Productos/putProductos.js
+++ server/src/controllers/Productos/putProductos.js
@@ -3,30 +3,30 @@ const { Productos } = require("../../db");
 const updateProductos = async (req, res) => {
   try {
     const { id } = req.params;
-    const { nombre, modelo, precio, stock, genero, marca, estado, imagen, comentarios, calificacion} = req.body;
-    if (!id) return res.status(400).json('Se necesita un id para poder modificar el producto');
+    const { nombre, modelo, precio, stock, genero, marca, estado, imagen } =
+      req.body;
+    if (!id)
+      return res
+        .status(400)
+        .json("Se necesita un id para poder modificar el producto");
 
     const producto = await Productos.findByPk(id);
     if (!producto) {
       return res.status(404).json({ error: "Producto no encontrado" });
     }
 
-    producto.nombre = nombre;
-    producto.modelo = modelo;
-    producto.precio = precio;
-    producto.stock = stock;
-    producto.genero = genero;
-    producto.marca = marca;
-    producto.estado = estado;
-    producto.imagen = imagen;
-    producto.comentarios = comentarios;
-    producto.calificacion = calificacion;
+    await producto.update({
+      nombre,
+      modelo,
+      precio,
+      stock,
+      genero,
+      marca,
+      estado,
+      imagen,
+    });
 
-    await producto.save();
-
-    return res
-      .status(200)
-      .json({ message: "Producto modificado exitosamente.", producto });
+    return res.status(200).json("Producto modificado con éxito!");
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: "Error al actualizar el producto" });
@@ -39,23 +39,22 @@ const borradoLogicoProductos = async (req, res) => {
     const { estado } = req.body;
 
     const producto = await Productos.findByPk(id);
-    if (!producto) { return res.status(404).json({ error: "Producto no encontrado" });}
+    if (!producto) {
+      return res.status(404).json({ error: "Producto no encontrado" });
+    }
 
     producto.estado = estado;
     //el estado corresponde al borrado lógico, en caso de que de que este activo deberia
     //de ser renderizado y en caso de que no, no deberia mostrarlo pero deberia ser renderizado para el admin
     await producto.save();
 
-    return res.status(200).json({ message: "Se ha modificado el estado del producto.", producto });
-
+    return res
+      .status(200)
+      .json({ message: "Se ha modificado el estado del producto.", producto });
   } catch (error) {
-
     console.error(error);
     return res.status(500).json({ error: "Error al actualizar el producto" });
   }
 };
-
-
-
 
 module.exports = { updateProductos, borradoLogicoProductos };

54e614ddac1b32d89133eadcaaa2dfd256d008a5
QuemeraT <adrianortizzt1vl@gmail.com>
2024-03-14 22:22:54 -0300

    Adri / agregue validaciones al formulario de creación de productos

--- client/src/components/AdminComponents/CreateForm/validation.js
+++ client/src/components/AdminComponents/CreateForm/validation.js
@@ -1,29 +1,43 @@
-const validate = valores => {
-    const errors = {};
+const validate = (valores) => {
+  const errors = {};
 
-    if(!valores.nombre){
-        errors.nombre = "Campo obligatorio"
-    }
+  const regexCaracteres = /^[a-zA-Z0-9\s]*$/;
 
-    if(!valores.marca){
-        errors.marca = "Campo obligatorio"
-    }
+  if (!valores.nombre) {
+    errors.nombre = "Campo obligatorio";
+  } else if (!regexCaracteres.test(valores.nombre)) {
+    errors.nombre = "Campo invalido";
+  } else if (valores.nombre.length > 15) {
+    errors.nombre = "El nombre puede contener 15 caracteres como máximo";
+  }
 
-    if(!valores.modelo){
-        errors.modelo = "Campo obligatorio"
-    }
+  if (!valores.marca) {
+    errors.marca = "Campo obligatorio";
+  } else if (!regexCaracteres.test(valores.marca)) {
+    errors.marca = "Campo invalido";
+  } else if (valores.marca.length > 15) {
+    errors.marca = "La marca puede contener 15 caracteres como máximo";
+  }
 
-    if(!valores.precio){
-        errors.precio = "Campo obligatorio"
-    }else if(typeof Number(valores.precio) !== 'number'){
-        errors.precio = "El precio tiene que ser un número"
-    }
+  if (!valores.modelo) {
+    errors.modelo = "Campo obligatorio";
+  } else if (!regexCaracteres.test(valores.modelo)) {
+    errors.modelo = "Campo invalido";
+  } else if (valores.modelo.length > 15) {
+    errors.modelo = "El modelo puede contener 15 caracteres como máximo";
+  }
 
-    if(valores.imagen.length === 0){
-        errors.imagen = "Se debe insertar una imagen como mínimo"
-    }
+  if (!valores.precio) {
+    errors.precio = "Campo obligatorio";
+  } else if (typeof Number(valores.precio) !== "number") {
+    errors.precio = "El precio tiene que ser un número";
+  }
 
-    return errors;
-}
+  if (valores.imagen.length === 0) {
+    errors.imagen = "Se debe insertar una imagen como mínimo";
+  }
+
+  return errors;
+};
 
 export default validate;

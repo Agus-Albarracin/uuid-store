409b3882190214a23907459618a4eaef4a4d292e
Agus-Albarracin <abusscin_@hotmail.com>
2024-03-14 22:52:08 -0300

    deploy

--- server/src/db.js
+++ server/src/db.js
@@ -3,8 +3,8 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const {DB_USER, DB_PASSWORD, DB_HOST, DB_URL} = process.env;
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/uuidstore`, {
-// const sequelize = new Sequelize(DB_URL, {
+// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/uuidstore`, {
+const sequelize = new Sequelize(DB_URL, {
   logging: false, 
   native: false, 
 });

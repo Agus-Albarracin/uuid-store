8f53197abb78c0281542652c9bf63a4007049dec
Agus-Albarracin <abusscin_@hotmail.com>
2024-03-17 00:09:52 -0300

    Agus/ se modifica carrita para agregar contador de ventas

--- server/src/controllers/Carrito/postCarrito.js
+++ server/src/controllers/Carrito/postCarrito.js
@@ -26,26 +26,18 @@ const postOrden = async (req, res) => {
     if (nombre) {
       let total = 0;
 
-      // for (const produ of productos) {
-      //   let producto = await Productos.findByPk(produ.id);
-      //   let stock = producto.getDataValue("stock");
-      //   stock = {
-      //     ...stock,
-      //     [produ.talle]: (stock[produ.talle] -= produ.cantidad),
-      //   };
-
-      //   await producto.update({ stock });
-      //   total += produ.precio * produ.cantidad;
-
-      // }
-
       for (const produ of productos) {
         let producto = await Productos.findByPk(produ.id);
         let stock = producto.getDataValue("stock");
+        let quantitysold = producto.getDataValue("quantitysold");
 
         // Construir un objeto que refleje la actualización del stock
         let updatedStock = { ...stock };
         updatedStock[produ.talle] -= produ.cantidad;
+
+        // Contar la cantidad de ventas por productos
+        let updateSold = {...quantitysold};
+        updateSold[produ.quantitysold] += produ.quantitysold;
 
         // Actualizar el stock en la base de datos
         await producto.update({ stock: updatedStock });
@@ -65,7 +57,7 @@ const postOrden = async (req, res) => {
 
         total,
 
-        userData: {
+        userData: { 
           //datos que corresponden a la compra (no necesariamente los del usuario)
           nombre,
           apellido,
@@ -119,120 +111,6 @@ const postOrden = async (req, res) => {
   }
 };
 
-// const postOrden = async (req, res) => {
-//   console.log(req.body);
-
-//   const {
-//     //info del cliente
-//     emailStorage,
-//     email,
-//     nombre,
-//     apellido,
-//     dni,
-//     numeroTramite,
-//     telefono,
-//     notificaciones,
-//     provincia,
-//     direccion,
-//     localidad,
-//     codigoPostal,
-//     total,
-//     estadoDelPedido,
-//     metodoDeEnvio,
-
-//     //info del producto
-//     productos,
-//   } = req.body;
-
-//   try {
-//     for (const Allproducto of productos) {
-//       const producto = await Productos.findByPk(Allproducto.id);
-
-//       if (!producto) {
-//         return res
-//           .status(400)
-//           .json({
-//             message: "No se encontró el producto con ID: " + Allproducto.id,
-//           });
-//       }
-
-// producto.quantitysold = (producto.quantitysold || 0) + 1;
-// await producto.save();
-//     }
-
-//     let usuario = await Usuario.findOne({ where: { email: emailStorage } });
-//     // if (!usuario) {
-//     //     return res.status(400).json({ message: "Usuario no encontrado" });
-//     // }
-
-//     // Crear un nuevo carrito en la base de datos
-//     const carrito = await Carrito.create({
-//       idDeCompra: uuid.v4(), // este es el id que se tiene que rastrear
-//       // email: email,
-//       productosEnCarrito: productos.map((producto) => ({
-//         ["Codigo: "]: producto.codigo,
-//       })),
-//       estadoDelPedido: estadoDelPedido,
-//       total: total,
-//       ProductoId: productos.map((prod) => prod.id),
-//       UsuarioId: usuario.id,
-//     });
-
-//     const ticketDeCompra = {
-//       productos,
-//       carrito,
-//       usuario: {
-//         id: usuario.id,
-//         nombre: nombre,
-//         apellido: apellido,
-//         email: email,
-//         dni: dni,
-//         telefono: telefono,
-//         provincia: provincia,
-//         direccion: direccion,
-//         localidad: localidad,
-//         codigoPostal: codigoPostal,
-//         metodoDeEnvio: metodoDeEnvio,
-//       },
-//     };
-
-//     const contenidoCorreo = `
-//               GRACIAS POR SU COMPRA!\n
-//               SE HA CONFIRMADO \n
-//               Y LA ESTAMOS PROCESANDO.\n
-//               \n
-//               Detalles del ticket de compra:
-//               \n
-//               Productos: ${ticketDeCompra.productos.map(
-//                 (produ) => produ.nombre + ""
-//               )}
-//               Usuario: ${ticketDeCompra.usuario.nombre} ${
-//       ticketDeCompra.usuario.apellido
-//     }
-//               Email: ${ticketDeCompra.usuario.email}
-//               DNI: ${ticketDeCompra.usuario.dni}
-//               Teléfono: ${ticketDeCompra.usuario.telefono}
-//               Provincia: ${ticketDeCompra.usuario.provincia}
-//               Dirección: ${ticketDeCompra.usuario.direccion}
-//               Localidad: ${ticketDeCompra.usuario.localidad}
-//               Código Postal: ${ticketDeCompra.usuario.codigoPostal}
-//               Método de Envío: ${ticketDeCompra.usuario.metodoDeEnvio}
-//               \n
-//               uuid.
-//           `;
-
-//     carrito.ticketDeCompra = JSON.stringify(ticketDeCompra);
-//     await carrito.save();
-
-//     // Llama a la función enviarCorreo
-//     enviarCorreo(email, "Gracias por su compra!", contenidoCorreo);
-
-//     return res.status(200).json(ticketDeCompra);
-//   } catch (error) {
-//     console.error("Error al crear la orden de compra:", error);
-//     res.status(500).json({ message: "Error interno del servidor" });
-//   }
-// };
 
 //Configuración de admin / transporter
 const transporter = nodemailer.createTransport({

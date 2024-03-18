090e97ea7ecc755aa2e14c9b54d639fc318fa2f9
Agus-Albarracin <abusscin_@hotmail.com>
2024-03-18 08:37:37 -0300

    estilos y deploy

--- client/src/Views/ConfirmacionDeCompra/ConfirmacionDeCompra.jsx
+++ client/src/Views/ConfirmacionDeCompra/ConfirmacionDeCompra.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-
 import styles from "./ConfirmacionDeCompra.module.scss";
-
 import UserData from "./UserData/UserData";
 import DireccionDeEnvio from "./DireccionDeEnvio/DireccionDeEnvio";
 import MetodoDeEnvio from "./MetodoDeEnvio/MetodoDeEnvio";
@@ -11,9 +9,7 @@ import Confirmacion from "./Confirmacion/Confirmacion";
 const ConfirmacionDeCompra = () => {
   const carritoJSON = window.localStorage.getItem("cart");
   const carrito = JSON.parse(carritoJSON);
-
   const userJSON = window.localStorage.getItem("loggedUser");
-
   const navigate = useNavigate();
 
   const [totalCarrito, setTotalCarrito] = useState(0);
@@ -46,7 +42,6 @@ const ConfirmacionDeCompra = () => {
   useEffect(() => {
     if (!userJSON || carrito.length === 0) navigate("/");
     window.localStorage.setItem("actualForm", JSON.stringify(userBuyData));
-
     setTotalCarrito(calcularTotal());
   }, []);
 
@@ -55,25 +50,21 @@ const ConfirmacionDeCompra = () => {
       <aside className={styles.carrito}>
         <h2 className={styles.header}>Tu compra</h2>
         <div className={styles.productos}>
-          {carrito.map((produ, index) => {
-            return (
-              <div className={styles.producto} key={index}>
-                <div>
-                  <h2>
-                    {produ.nombre} {produ.modelo} {produ.marca}
-                  </h2>
-                  <h2><span className="font-bold">Precio:</span> ${produ.precio}</h2>
-                  <h2><span className="font-bold">Talle:</span> {produ.talle}</h2>
-                  <h2><span className="font-bold">Cantidad:</span> {produ.cantidad}</h2>
-                </div>
-                <hr />
+          {carrito.map((produ, index) => (
+            <div className={styles.producto} key={index}>
+              <div>
+                <h2><span className="font-bold" style={{ color: 'red' }}>{produ.nombre}</span> </h2>
+                <h2><span className="font-bold">Precio:</span> ${produ.precio}</h2>
+                <h2><span className="font-bold">Talle:</span> {produ.talle}</h2>
+                <h2><span className="font-bold">Cantidad:</span> {produ.cantidad}</h2>
+                <img src={produ.imagen[0]} alt="producto"></img>
               </div>
-            );
-          })}
+            </div>
+          ))}
         </div>
-
         <div className={styles.total}>Total: ${totalCarrito}</div>
       </aside>
+
       <div className={styles.data}>
         <div className={styles.index}>
           <h2>CONFIRMACION DE LA COMPRA</h2>
@@ -81,37 +72,25 @@ const ConfirmacionDeCompra = () => {
             <ul>
               <li
                 className={view > 1 ? styles.disponible : styles.noDisponible}
-                onClick={() => {
-                  handleView(1);
-                }}
-              >
+                onClick={() => {handleView(1);}}>
                 <strong>1.</strong> Tus datos {">"}
               </li>
 
               <li
                 className={view > 2 ? styles.disponible : styles.noDisponible}
-                onClick={() => {
-                  view > 2 && handleView(2);
-                }}
-              >
+                onClick={() => {view > 2 && handleView(2);}}>
                 <strong>2.</strong> Dirección de envío {">"}
               </li>
 
               <li
                 className={view > 3 ? styles.disponible : styles.noDisponible}
-                onClick={() => {
-                  view > 3 && handleView(3);
-                }}
-              >
+                onClick={() => {view > 3 && handleView(3);}}>
                 <strong>3.</strong> Método de envío {">"}
               </li>
 
               <li
                 className={view > 4 ? styles.disponible : styles.noDisponible}
-                onClick={() => {
-                  view > 4 && handleView(4);
-                }}
-              >
+                onClick={() => {view > 4 && handleView(4);}}>
                 <strong>4.</strong> Confirmación de compra
               </li>
             </ul>
@@ -148,3 +127,4 @@ const ConfirmacionDeCompra = () => {
 };
 
 export default ConfirmacionDeCompra;
+

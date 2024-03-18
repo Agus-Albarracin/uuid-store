aa692b90caadf0fb39c44e1137a356e052ddacc0
Agus-Albarracin <abusscin_@hotmail.com>
2024-03-18 03:48:20 -0300

    Agus/ modificaciones al delete

--- server/src/controllers/Users/deleteUser.js
+++ server/src/controllers/Users/deleteUser.js
@@ -1,15 +1,18 @@
 const { Usuario } = require("../../db");
+const nodemailer = require("nodemailer");
+
 
 const deleteUser = async (req, res) => {
   try {
-    const { email } = req.body;
+    const { email } = req.body ;
     if(!email){ return res.status(404).json("Se necesita el email para poder eliminar el usuario.")}
     
     const usuario = await Usuario.findOne({where: {email}});
 
     if (!usuario) {return res.status(404).json({ message: "Usuario no encontrado." });}
+    
     await usuario.destroy();
-
+    enviarCorreo(email, "ESPERO VOLVERTE A VER", "Se elimino con exito tu cuenta.")
     return res.status(200).json({ message: "Usuario eliminado exitosamente.", usuario });
 
   } catch (error) {
@@ -17,5 +20,36 @@ const deleteUser = async (req, res) => {
     return res.status(500).json({ message: "Error al eliminar el usuario." });
   }
 };
+
+
+//Configuración de admin / transporter
+const transporter = nodemailer.createTransport({
+  service: "Outlook",
+  auth: {
+    user: "uuidstore@outlook.com",
+    pass: "Henry!123",
+  },
+});
+
+// Función para enviar el correo electrónico.
+function enviarCorreo(destinatario, asunto, mensaje) {
+  // Opciones del correo electrónico
+  const mailOptions = {
+    from: "uuidstore@outlook.com",
+    to: destinatario,
+    subject: asunto,
+    text: mensaje,
+  };
+
+  // Enviar el correo electrónico
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log("Error al enviar el correo electrónico:", error);
+    } else {
+      console.log("Correo electrónico enviado:", info.response);
+    }
+  });
+}
+
 
 module.exports = { deleteUser };
